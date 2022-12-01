6bb35b36654f1fac9d97d88965e263ccf3611e02
manisharpatil <manisharpatil6@gmail.com>
2022-12-01 23:52:10 +0530

    feat: added route in app.js

--- src/App.js
+++ src/App.js
@@ -5,9 +5,12 @@ const App = () => {
   <BrowserRouter>
     <Box sx={{ backgroundColor: '#000'}}>
     Navbar
-    <Routes>
-      <Route path="/" exact element={} />
-    </Routes>
+      <Routes>
+        <Route path="/" exact element={<Feed />} />
+        <Route path="/video/:id" element={<VideoDetail />} />
+        <Route path="/channel/:id" element={<ChannelDetail />} />
+        <Route path="/search/:searchTerm" element={<SearchFeed />} />
+      </Routes>
     </Box>
   </BrowserRouter>
 }
